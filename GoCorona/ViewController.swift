c583ff58701f55b323a40a80b49b57badf22ed96
Shashank Chandran <iamcrazyshank@gmail.com>
2020-04-30 13:53:58 +0100

    Initial Commit

---
+++ GoCorona/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoCorona
+//
+//  Created by Shashank Chandran on 4/30/20.
+//  Copyright © 2020 Shashank Chandran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
